cc3e81abef3a171163e881603f258693c4efc4ec
atonu <atonu.zahin@gmail.com>
2020-08-20 03:43:35 +0600

    mongo integration update.

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const bodyparse = require('body-parser');
 
 const mongoose = require('./db.js');
+var employeeController = require('./controllers/employeeController')
 
 var app = express();
 app.use(bodyparse.json());
 
 app.listen(3000, () => console.log('connected@3000'));
+
+app.use('/employees', employeeController)
