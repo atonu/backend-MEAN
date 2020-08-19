7a3a132fb578369b5720d3fb00d46acda87b2c66
atonu <atonu.zahin@gmail.com>
2020-08-20 04:42:15 +0600

    crud done

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ObjectId = require('mongoose').Types.ObjectId;
 var router = express.Router();
 
 var {Employee} = require ('../models/employee')
@@ -11,6 +12,19 @@ router.get('/', (req,res) => {
     });
 })
 
+router.get('/:id', (req,res) => {
+
+    if(!ObjectId.isValid(req.param.id)){
+        console.log(`Invalid id ${req.params.id}`);
+        // return res.status(400).send(`No record with ID: ${req.params.id}`);
+    }
+
+    Employee.findById(req.params.id, (err,doc) => {
+        if (!err) { res.send(doc)}
+        else {console.log('Error getting employee:' + JSON.stringify(err,undefined,2));}
+    })
+})
+
 router.post('/', (req,res) => {
     var emp = new Employee({
         name: req.body.name,
@@ -19,6 +33,36 @@ router.post('/', (req,res) => {
         salary: req.body.salary,
     });
     emp.save((err,doc) => {
+        if (!err) {res.send(doc)}
+        else{console.log('Error: ' + JSON.stringify(err,undefined,2))};
+    });
+})
+
+router.put('/:id', (req,res) => {
+    if(!ObjectId.isValid(req.param.id)){
+        console.log(`Invalid id ${req.params.id}`);
+        // return res.status(400).send(`No record with ID: ${req.params.id}`);
+    }
+
+    var emp = {
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary,
+    };
+    Employee.findByIdAndUpdate(req.params.id, {$set: emp}, {new: true}, (err,doc) => {
+        if (!err) {res.send(doc)}
+        else{console.log('Error: ' + JSON.stringify(err,undefined,2))};
+    });
+})
+
+router.delete('/:id', (req,res) => {
+    if(!ObjectId.isValid(req.param.id)){
+        console.log(`Invalid id ${req.params.id}`);
+        // return res.status(400).send(`No record with ID: ${req.params.id}`);
+    }
+
+    Employee.findByIdAndDelete(req.params.id, (err,doc) => {
         if (!err) {res.send(doc)}
         else{console.log('Error: ' + JSON.stringify(err,undefined,2))};
     });
